855972f51c16334e01a814b7da21e709274805f2
Asad <asadalipuh5@gmail.com>
2025-02-09 01:27:26 +0500

    Changes Made

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 console.log("Lets Write Javascript")
+let currentSong = new Audio();
 
+function secondsToMinutesSeconds(seconds){
+    if (isNaN(seconds)|| seconds<0){
+        return "00:00";
+    }
+
+    const minutes = Math.floor(seconds / 60)
+    const remainingSeconds = Math.floor(seconds % 60)
+
+    const formattedMinutes = String(minutes).padStart(2, "0")
+    const formattedSeconds = String(remainingSeconds).padStart(2, "0")
+
+    return `${formattedMinutes}:${formattedSeconds}`
+}
 
 async function getSongs() {
 
@@ -22,11 +36,24 @@ async function getSongs() {
 
 }
 
+const playMusic = (track, pause=false) => { 
+    // let audio = new Audio("/songs/"+track)
+    currentSong.src = "/songs/" + track
+    if(!pause){
+        currentSong.play()
+        play.src = "http://127.0.0.1:3000/img/pause.svg"
+    }
+    document.querySelector(".songinfo").innerHTML = decodeURI(track)
+    document.querySelector(".songtime").innerHTML = "00:00 / 00:00" 
+}
+
 async function main() {
 
-    let currentSong;
+    
     // Get the list of all songs
     let songs = await getSongs();
+    playMusic(songs[0], true)
+
     console.log(songs);
 
     let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0];
@@ -48,13 +75,51 @@ async function main() {
     }
 
     // Attach an event Listener to each Song
-    Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(
-        (e) => { console.log(e.getElementsByTagName("div")[0]) }
+    Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach((e) => {e.addEventListener("click", (element) => { 
+            console.log(e.querySelector(".info").firstElementChild.innerHTML)
+            playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
+         })
+         }
     )
 
+    // Attach and event listener to play, next and previous
+    play.addEventListener("click", () => { 
+        if(currentSong.paused){
+            currentSong.play()
+            play.src = "http://127.0.0.1:3000/img/pause.svg"
+        }
+        else{
+            currentSong.pause()
+            play.src = "http://127.0.0.1:3000/img/play.svg"
+        }
+    })
 
+    // Listen for timeupdated event
+    currentSong.addEventListener("timeupdate", () => { 
+        console.log(currentSong.currentTime, currentSong.duration);
+        document.querySelector(".songtime").innerHTML = `
+        ${secondsToMinutesSeconds(currentSong.currentTime)}/
+        ${secondsToMinutesSeconds(currentSong.duration)}`
+        document.querySelector(".circle").style.left = (currentSong.currentTime/currentSong.duration) * 100 + "%"
+    })
+
+    // Add an Event Listener to Seekbar
+    document.querySelector(".seekbar").addEventListener("click", (e) => { 
+        let percent = (e.offsetX/e.target.getBoundingClientRect().width) * 100
+        document.querySelector(".circle").style.left = percent + "%"
+        currentSong.currentTime = ((currentSong.duration) * percent) /100
+    })
+    // Add an event Listener to Hemburger
+    document.querySelector(".hamburger").addEventListener("click", () => { 
+        document.querySelector(".left").style.left = "0"
+        console.log("hi")
+    })
+
+    // Add and event Listener for close button in Hemburger
+    document.querySelector(".close").addEventListener("click",() => { 
+        document.querySelector(".left").style.left = "-120%"
+    })
     
-
 }
 
 main();
